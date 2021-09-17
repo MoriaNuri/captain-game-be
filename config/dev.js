b5a9fd175b4daa5fcc0fc9af5b9738628d433c34
MoriaNuri <moriakorsia@gmail.com>
2021-09-17 15:08:26 +0300

    atlas_db

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  'dbURL': 'mongodb://localhost:27017',
+  // 'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://MoriaNuri:knMvRY$r4Yu3VVa@cluster0.6q7fj.mongodb.net/assafMedia_db?retryWrites=true&w=majority',
 
 }
